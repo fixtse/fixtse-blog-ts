d3bfb76b03332b93c5067d8b847e70cccd6570b9
Sergio Romero <sromerop@utp.edu.pe>
2022-11-22 23:35:18 -0500

    Second Post

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -10,6 +10,7 @@ import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 import { ReactNode } from 'react'
 import { PostFrontMatter } from 'types/PostFrontMatter'
 import { AuthorFrontMatter } from 'types/AuthorFrontMatter'
+import YouTube from '@/components/Youtube'
 
 const editUrl = (fileName) => `${siteMetadata.siteRepo}/tree/main/data/blog/${fileName}`
 const discussUrl = (slug) =>
@@ -33,7 +34,7 @@ interface Props {
 }
 
 export default function PostLayout({ frontMatter, authorDetails, next, prev, children }: Props) {
-  const { slug, fileName, date, title, tags } = frontMatter
+  const { slug, fileName, date, title, tags, id } = frontMatter
 
   return (
     <SectionContainer>
@@ -104,6 +105,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
+                <YouTube id={id} />
                 <Link href={discussUrl(slug)} rel="nofollow">
                   {'Discutir en twitter'}
                 </Link>
