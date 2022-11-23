ddfc7ce2b6529d1d448ded310d925a66935c3d80
Sergio Romero <sromerop@utp.edu.pe>
2022-11-22 22:51:40 -0500

    Fix Array Images SEO

--- types/PostFrontMatter.ts
+++ types/PostFrontMatter.ts
@@ -5,7 +5,7 @@ export type PostFrontMatter = {
   lastmod?: string
   draft?: boolean
   summary?: string
-  image: string
+  images: string[]
   authors?: string[]
   layout?: string
   canonicalUrl?: string
