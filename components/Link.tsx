9afe72b30f410ce363fe7f7033bbeb6363ff3057
Sergio Romero <fixtse@gmail.com>
2023-06-18 18:27:29 -0500

    build: :package: update new next link

--- components/Link.tsx
+++ components/Link.tsx
@@ -3,25 +3,21 @@ import Link from 'next/link'
 import { AnchorHTMLAttributes, DetailedHTMLProps } from 'react'
 
 const CustomLink = ({
-  href,
-  ...rest
+	href,
+	...rest
 }: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => {
-  const isInternalLink = href && href.startsWith('/')
-  const isAnchorLink = href && href.startsWith('#')
+	const isInternalLink = href && href.startsWith('/')
+	const isAnchorLink = href && href.startsWith('#')
 
-  if (isInternalLink) {
-    return (
-      <Link href={href}>
-        <a {...rest} />
-      </Link>
-    )
-  }
+	if (isInternalLink) {
+		return <Link href={href} {...rest}></Link>
+	}
 
-  if (isAnchorLink) {
-    return <a href={href} {...rest} />
-  }
+	if (isAnchorLink) {
+		return <a href={href} {...rest} />
+	}
 
-  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
+	return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
 }
 
 export default CustomLink
--- components/Tag.tsx
+++ components/Tag.tsx
@@ -3,31 +3,31 @@ import Link from 'next/link'
 import kebabCase from '@/lib/utils/kebabCase'
 
 interface Props {
-  text: string
-  num: number
+	text: string
+	num: number
 }
 
 const hasValue = (text: string, num: number) => {
-  if (num > 0) {
-    return text.split(' ').join('-') + ` (${num})`
-  } else {
-    return text.split(' ').join('-')
-  }
+	if (num > 0) {
+		return text.split(' ').join('-') + ` (${num})`
+	} else {
+		return text.split(' ').join('-')
+	}
 }
 
 const Tag = ({ text, num }: Props) => {
-  return (
-    <Link href={`/tags/${kebabCase(text)}`}>
-      <div className="pt-3">
-        <div className="group relative">
-          <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-fuchsia-600 to-primary-600 opacity-25 blur transition duration-500 group-hover:opacity-100 group-hover:duration-200"></div>
-          <a className="relative mr-3 flex rounded-lg bg-primary-200 py-2 px-2 text-sm font-semibold uppercase leading-none text-primary-900 dark:bg-primary-600 dark:text-primary-100">
-            {hasValue(text, num)}
-          </a>
-        </div>
-      </div>
-    </Link>
-  )
+	return (
+		<Link href={`/tags/${kebabCase(text)}`} legacyBehavior>
+			<div className="pt-3">
+				<div className="group relative">
+					<div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-fuchsia-600 to-primary-600 opacity-25 blur transition duration-500 group-hover:opacity-100 group-hover:duration-200"></div>
+					<a className="relative mr-3 flex rounded-lg bg-primary-200 py-2 px-2 text-sm font-semibold uppercase leading-none text-primary-900 dark:bg-primary-600 dark:text-primary-100">
+						{hasValue(text, num)}
+					</a>
+				</div>
+			</div>
+		</Link>
+	)
 }
 
 export default Tag
